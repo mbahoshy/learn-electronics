ec6de0f291db4c9722f5dfcac3ce9ce1cd46aea8
mbahoshy <mbahoshy@gmail.com>
2013-12-03 12:37:34 -0800

    index css... again

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,14 @@
 		$(document).ready(function(){
 			$('#sign_in').on('click', function(){
-				$('#sign_in_form').fadeToggle();
+				$('#sign_in_inputs').fadeToggle();
+				$('#sign_in').addClass('sign-in-active');
+				$('#sign_in').off('click');
+				$('#sign_in').on('click', function () {
+					// $(this).parent().form.submit();
+					$('#sign_in_form').submit();
+					console.dir($(this).parent().form);
+					// this.form.submit();
+				});
 			});
 
 			$('#sign_up').on('click', function(){
