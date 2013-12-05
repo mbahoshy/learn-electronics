bd59be97d17158e66cae17a8c134f023132cd9b7
mbahoshy <mbahoshy@gmail.com>
2013-12-05 13:19:45 -0800

    minor css

--- public/js/views/lessonview.js
+++ public/js/views/lessonview.js
@@ -2,7 +2,7 @@ TRADE.LessonView = Backbone.View.extend({
         tagName: 'div',
         className: '',
         template: _.template("<a class='no-decoration' href='#slides/<%= lessonid %>'><div class='lesson-list'><h1><%= name %></h1><h2><%= snippet %></h2></div></a>"),
-        problemtemplate: _.template("<a class='no-decoration' href='#slides/<%= lessonid %>'><div class='lesson-list problem-list'><h1><%= name %></h1><h2><%= snippet %></h2></div></a>"),
+        problemtemplate: _.template("<a class='no-decoration' href='#slides/<%= lessonid %>'><div class='lesson-list'><h1><%= name %></h1><h2><%= snippet %></h2></div></a>"),
         events: {
                 "mouseover": "lessonMouseover",
                 "mouseout": "lessonMouseout"
--- public/js/views/usercardview.js
+++ public/js/views/usercardview.js
@@ -1,6 +1,6 @@
 TRADE.UserView = Backbone.View.extend({
         tagName: 'div',
-        className: 'user-card',
+        className: 'user-card fright',
         template: _.template("<h2>Hello, <%= firstName %></h2><p><b>Rank:</b> <%= level %></p><p><b>Lessons Completed:</b> <%= _.where(progress, {classid: classid, completed:true, lessontype:'lesson'}).length %></p><p><b>Problems Completed:</b> <%= _.where(progress, {classid: classid, completed:true, lessontype:'problem'}).length %></p>"),
         events: {},
 
