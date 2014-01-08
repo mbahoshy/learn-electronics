e28cb3cdd3933ffc03d4cfc0bce14c92bae56f8c
Matt <mbahoshy@gmail.com>
2014-01-08 09:55:24 -0800

    fixed login on landing page

--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,24 @@
 			// $('#submit_button').on('click', function (e) {
 				
 			// })
-			$('#submit_button').on('click', function(e){
+
+			$('#sign_in').on('click', function(e){
 				e.preventDefault();
-				$('#sign_in_inputs').fadeToggle();
-				$('#sign_in').addClass('sign-in-active');
-				$('#submit_button').off('click');
-				$('#submit_button').on('click', function () {
-					// $(this).parent().form.submit();
-					$('#sign_in_form').submit();
-					console.dir($(this).parent().form);
-					// this.form.submit();
-				});
+				$('#sign_in_form').fadeToggle();
 			});
+			
+			// $('#submit_button').on('click', function(e){
+			// 	e.preventDefault();
+			// 	$('#sign_in_inputs').fadeToggle();
+			// 	$('#sign_in').addClass('sign-in-active');
+			// 	$('#submit_button').off('click');
+			// 	$('#submit_button').on('click', function () {
+			// 		// $(this).parent().form.submit();
+			// 		$('#sign_in_form').submit();
+			// 		console.dir($(this).parent().form);
+			// 		// this.form.submit();
+			// 	});
+			// });
 
 			$('#sign_up').on('click', function(){
 				$('#sign_up_form').toggle();
