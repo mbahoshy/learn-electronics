260d3962be99df307cd75c552431534ef3b51e04
Matt <mbahoshy@gmail.com>
2014-01-13 21:39:43 -0800

    added further functionality to test feature. Submit button now turning on and off. all ready to post data to server and render next question

--- public/js/views/testslidesview.js
+++ public/js/views/testslidesview.js
@@ -3,11 +3,13 @@ TRADE.TestSlides = Backbone.View.extend({
         className: 'test-holder',
         template: _.template("<div class='question-txt'><%= questiontxt %></div>"),
         optiontemplate: _.template("<div data-optionid='<%= optionid %>' class='option-txt'><%= optionid %>. <%= optiontxt %></div>"),
-        navtemplate: _.template("<div class='option-nav'>QUESTION <%= (currentquestion + 1) %> OF <%= numquestions %>"),
+        navtemplate: _.template("<div class='option-nav'>QUESTION <%= (currentquestion + 1) %> OF <%= numquestions %></div>"),
+        submittemplate: _.template("<div class='option-submit'><div id='submit_option'>Submit</div></div>"),
         events: {
                 "mouseover .option-txt": "lessonMouseover",
                 "mouseout .option-txt": "lessonMouseout",
-                "click .option-txt": "selectOption"
+                "click .option-txt": "selectOption",
+                "click .active-submit-option": "submitOption"
         },
         lessonMouseover: function (e) {
                 $(e.currentTarget).addClass('option-hover');
@@ -19,15 +21,24 @@ TRADE.TestSlides = Backbone.View.extend({
         },
         selectOption: function (e) {
                 var answer = $(e.currentTarget).data('optionid');
-            console.log(answer);
+                $('.active-option').removeClass('active-option');
+                $(e.currentTarget).addClass('active-option');
+                $('#submit_option').addClass('active-submit-option');
+                $('#submit_option').data('optionid', answer);
+                console.log(answer);
+        },
+        submitOption: function (e) {
+                var optionid = $('#submit_option').data('optionid');
+                console.dir(optionid);
         },
         render : function (questionindex) {
                 console.dir(this.collection.models[questionindex]);
                 var questiontxt = this.collection.models[questionindex].attributes.qtxt;
-                var numquestions = this.collection.models[questionindex].attributes.options.length;
+                var numquestions = this.collection.models.length;
                 this.$el.append(this.template({questiontxt: questiontxt}));
                 var options = this.collection.models[questionindex].attributes.options;
                 options.forEach(this.renderOptions, this);
+                this.$el.append(this.submittemplate);
                 this.$el.append(this.navtemplate({currentquestion: questionindex, numquestions: numquestions}));
         },
 
